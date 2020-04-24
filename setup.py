8501cb8e37cd07cfd42fb2dd47b092c853de5fe1
lytex <julianlpc15@gmail.com>
2020-04-25 00:05:20 +0200

    execute pydo install to create database after the installation is finished

--- setup.py
+++ setup.py
@@ -1,6 +1,42 @@
 from setuptools import find_packages
 from setuptools import setup
 
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+import os
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        try:
+            os.makedirs('~/.local/share/pydo')
+            os.system('pydo install')
+        except FileExistsError:
+            print('Installation directory already exits')
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for develop mode."""
+    def run(self):
+        develop.run(self)
+        try:
+            os.makedirs('~/.local/share/pydo')
+            os.system('pydo install')
+        except FileExistsError:
+            print('Installation directory already exits')
+
+class PostEggInfoCommand(egg_info):
+    """Post-installation for egg_info mode."""
+    def run(self):
+        egg_info.run(self)
+        try:
+            os.makedirs('~/.local/share/pydo')
+            os.system('pydo install')
+        except FileExistsError:
+            print('Installation directory already exits')
+
 __version__ = '0.1.0'
 
 setup(
@@ -17,6 +53,11 @@ setup(
         'migrations/versions/*',
     ]},
     entry_points={'console_scripts': ['pydo = pydo:main']},
+    cmdclass={
+        'install': PostInstallCommand,
+        'develop': PostDevelopCommand,
+        'egg_info': PostEggInfoCommand,
+    },
     install_requires=[
         "argcomplete>=1.11.1",
         "SQLAlchemy>=1.3.11",
